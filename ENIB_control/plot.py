39ffdc3bbcaee7c5dc84f3e17691450b5c846c2d
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2020-03-23 12:05:31 +0100

    Python formatting

--- ENIB_control/plot.py
+++ ENIB_control/plot.py
@@ -4,143 +4,204 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from .utils import get_T, nichols_grid
 
-color_list = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",  "#EF553B", "brown"]   
+color_list = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#EF553B", "brown"]
 
 # Utility Function
-def default_layout(xlabel,ylabel,name):
-        layout =   {"xaxis":{"title":{"text":xlabel}},
-                    "yaxis":{"title":{"text":ylabel}},
-                    "title":{"text":name}
-                }
-        return layout
+def default_layout(xlabel, ylabel, name):
+    layout = {
+        "xaxis": {"title": {"text": xlabel}},
+        "yaxis": {"title": {"text": ylabel}},
+        "title": {"text": name},
+    }
+    return layout
 
 
-def impulse(tf_list=[],N=200,T=None,name=None):
-    
+def impulse(tf_list=[], N=200, T=None, name=None):
+
     data = []
-    T = get_T(tf_list,N=N,T=T)
+    T = get_T(tf_list, N=N, T=T)
 
-    for index,tf in enumerate(tf_list):
-        t,y = ctl.impulse_response(tf, T=T)
-        tf_name = "tf {}".format(index+1)
-        data.append({"x":t,"y":y,"name":tf_name,"showlegend":False})
+    for index, tf in enumerate(tf_list):
+        t, y = ctl.impulse_response(tf, T=T)
+        tf_name = "tf {}".format(index + 1)
+        data.append({"x": t, "y": y, "name": tf_name, "showlegend": False})
 
-    layout = default_layout("time (s)","response",name)
-    fig = go.Figure(data,layout=layout)
+    layout = default_layout("time (s)", "response", name)
+    fig = go.Figure(data, layout=layout)
     fig.show()
 
 
-def step(tf_list=[],N=200,T=None,name=None):
-    
+def step(tf_list=[], N=200, T=None, name=None):
+
     data = []
-    T = get_T(tf_list,N=N,T=T)
-        
-    for index,tf in enumerate(tf_list):
-        t,y = ctl.step_response(tf, T=T)
-        tf_name = "tf {}".format(index+1)
-        data.append({"x":t,"y":y,"name":tf_name,"showlegend":False})
-    
-    layout = default_layout("time (s)","response",name)
-    fig = go.Figure(data,layout=layout)
+    T = get_T(tf_list, N=N, T=T)
+
+    for index, tf in enumerate(tf_list):
+        t, y = ctl.step_response(tf, T=T)
+        tf_name = "tf {}".format(index + 1)
+        data.append({"x": t, "y": y, "name": tf_name, "showlegend": False})
+
+    layout = default_layout("time (s)", "response", name)
+    fig = go.Figure(data, layout=layout)
     fig.show()
 
 
 # ZERO POLE PLOT
-def pzmap(tf_list=[],name=None,layout=None):
-    
-    hovertemplate_pole = "<b>Pole<b><br><b>real</b>: %{x:.3f}<br><b>imag</b>: %{y:.3f}<br>"
-    hovertemplate_zero = "<b>Zero<b><br><b>real</b>: %{x:.3f}<br><b>imag</b>: %{y:.3f}<br>"
+def pzmap(tf_list=[], name=None, layout=None):
+
+    hovertemplate_pole = (
+        "<b>Pole<b><br><b>real</b>: %{x:.3f}<br><b>imag</b>: %{y:.3f}<br>"
+    )
+    hovertemplate_zero = (
+        "<b>Zero<b><br><b>real</b>: %{x:.3f}<br><b>imag</b>: %{y:.3f}<br>"
+    )
 
     data = []
     max = 0
-    for index,tf in enumerate(tf_list):
+    for index, tf in enumerate(tf_list):
         line_pole = dict(color=color_list[index])
         line_zero = dict(color=color_list[index])
         z = tf.zero()
         p = tf.pole()
-        
-        tf_name = "tf {}".format(index+1)
-        data.append({"x":np.real(p),"y":np.imag(p),"name":tf_name,"line":line_pole,"hovertemplate": hovertemplate_pole, "mode": "markers","marker":{"symbol":"x","size":8}})
-        data.append({"x":np.real(z),"y":np.imag(z),"name":tf_name,"line":line_zero,"hovertemplate": hovertemplate_zero, "mode": "markers","marker":{"symbol":"circle","size":8}})
+
+        tf_name = "tf {}".format(index + 1)
+        data.append(
+            {
+                "x": np.real(p),
+                "y": np.imag(p),
+                "name": tf_name,
+                "line": line_pole,
+                "hovertemplate": hovertemplate_pole,
+                "mode": "markers",
+                "marker": {"symbol": "x", "size": 8},
+            }
+        )
+        data.append(
+            {
+                "x": np.real(z),
+                "y": np.imag(z),
+                "name": tf_name,
+                "line": line_zero,
+                "hovertemplate": hovertemplate_zero,
+                "mode": "markers",
+                "marker": {"symbol": "circle", "size": 8},
+            }
+        )
         # change max
-        max_temp = np.max(np.hstack((np.abs(z),np.abs(p))))
-        if max_temp > max :
+        max_temp = np.max(np.hstack((np.abs(z), np.abs(p))))
+        if max_temp > max:
             max = max_temp
 
     # create PLotly figure
-    layout = default_layout("Real","Imag",name)
-    layout["xaxis"]["range"] = [-1.5*max,1.5*max]
+    layout = default_layout("Real", "Imag", name)
+    layout["xaxis"]["range"] = [-1.5 * max, 1.5 * max]
     layout["yaxis"]["scaleanchor"] = "x"
     layout["yaxis"]["scaleratio"] = 1
-    fig = go.Figure(data,layout=layout)
+    fig = go.Figure(data, layout=layout)
     fig.show()
 
 
 # BODE PLOT
-def bode(tf_list=[],omega=None,name=None):
+def bode(tf_list=[], omega=None, name=None):
 
     hovertemplate_mag = "<b>w</b>: %{x:.3f} rad/s<br><b>mag</b>: %{y:.3f} dB<br><b>phase</b>: %{text:.3f} deg<br>"
     hovertemplate_phase = "<b>w</b>: %{x:.3f} rad/s<br><b>mag</b>: %{text:.3f} dB<br><b>phase</b>: %{y:.3f} deg<br>"
-    
-    fig = make_subplots(rows=2, cols=1,shared_xaxes=True)
 
-    
-    for index,tf in enumerate(tf_list):
-        
-        mag_list,phase_list,omega = ctl.bode_plot(tf, omega=omega,Plot=False, omega_limits=None, omega_num=None,margins=None)
-        mag = 20*np.log10(mag_list)
-        phase = phase_list*180/np.pi
-        tf_name = "tf {}".format(index+1)
-        data_mag= {"x":omega,"y":mag,"name":tf_name,"hovertemplate": hovertemplate_mag ,"text":phase,"showlegend":False}
-        data_phase = {"x":omega,"y":phase,"name":tf_name,"hovertemplate": hovertemplate_phase,"text":mag,"showlegend":False}
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
 
-        #add to plotly
-        fig.add_trace(data_mag,row=1, col=1)
-        fig.add_trace(data_phase,row=2, col=1)
-    
-    fig.update_yaxes(title_text="Magnitude",row=1, col=1)
-    fig.update_xaxes(title_text="w (rad/s)",type="log",row=1, col=1)
-    fig.update_yaxes(title_text="Phase",row=2, col=1)
-    fig.update_xaxes(title_text="w (rad/s)",type="log",row=2, col=1)
+    for index, tf in enumerate(tf_list):
+
+        mag_list, phase_list, omega = ctl.bode_plot(
+            tf, omega=omega, Plot=False, omega_limits=None, omega_num=None, margins=None
+        )
+        mag = 20 * np.log10(mag_list)
+        phase = phase_list * 180 / np.pi
+        tf_name = "tf {}".format(index + 1)
+        data_mag = {
+            "x": omega,
+            "y": mag,
+            "name": tf_name,
+            "hovertemplate": hovertemplate_mag,
+            "text": phase,
+            "showlegend": False,
+        }
+        data_phase = {
+            "x": omega,
+            "y": phase,
+            "name": tf_name,
+            "hovertemplate": hovertemplate_phase,
+            "text": mag,
+            "showlegend": False,
+        }
+
+        # add to plotly
+        fig.add_trace(data_mag, row=1, col=1)
+        fig.add_trace(data_phase, row=2, col=1)
+
+    fig.update_yaxes(title_text="Magnitude", row=1, col=1)
+    fig.update_xaxes(title_text="w (rad/s)", type="log", row=1, col=1)
+    fig.update_yaxes(title_text="Phase", row=2, col=1)
+    fig.update_xaxes(title_text="w (rad/s)", type="log", row=2, col=1)
     fig.show()
 
 
-def nichols(tf_list=[],omega=None,show_mag_grid=True,show_phase_grid=False,cl_mags = None,cl_phases = None, name=None):
+def nichols(
+    tf_list=[],
+    omega=None,
+    show_mag_grid=True,
+    show_phase_grid=False,
+    cl_mags=None,
+    cl_phases=None,
+    name=None,
+):
 
     xlabel = "Phase (deg)"
     ylabel = "Magnitude (dB)"
     hovertemplate = "<b>w</b>: %{text:.3f} rad/s<br><b>mag</b>: %{y:.3f} dB<br><b>phase</b>: %{x:.3f} deg<br>"
-    
+
     data_mag = []
     data_phase = []
     data = []
-    
-    for index,tf in enumerate(tf_list):
-        
-        mag_list,phase_list,omega = ctl.bode_plot(tf, omega=omega,Plot=False, omega_limits=None, omega_num=None,margins=None)
-        mag = 20*np.log10(mag_list)
-        phase = phase_list*180/np.pi
-        tf_name = "tf {}".format(index+1)
-        data.append({"x":phase,"y":mag,"name":tf_name,"hovertemplate": hovertemplate ,"text":omega,"showlegend":False})
+
+    for index, tf in enumerate(tf_list):
+
+        mag_list, phase_list, omega = ctl.bode_plot(
+            tf, omega=omega, Plot=False, omega_limits=None, omega_num=None, margins=None
+        )
+        mag = 20 * np.log10(mag_list)
+        phase = phase_list * 180 / np.pi
+        tf_name = "tf {}".format(index + 1)
+        data.append(
+            {
+                "x": phase,
+                "y": mag,
+                "name": tf_name,
+                "hovertemplate": hovertemplate,
+                "text": omega,
+                "showlegend": False,
+            }
+        )
 
     # create PLotly figure
-    layout = default_layout("Phase (deg)","Magnitude (dB)",name)
-    fig = go.Figure(data,layout=layout)
+    layout = default_layout("Phase (deg)", "Magnitude (dB)", name)
+    fig = go.Figure(data, layout=layout)
 
     # add contours
-    mag_list, phase_list = nichols_grid(cl_mags,cl_phases)
-    if show_mag_grid :
-        line_mag = dict(color = '#555', width = 1,dash = "dot")
-        
+    mag_list, phase_list = nichols_grid(cl_mags, cl_phases)
+    if show_mag_grid:
+        line_mag = dict(color="#555", width=1, dash="dot")
+
         for mag in mag_list:
-            fig.add_trace(go.Scatter(mag,hoverinfo="name",showlegend=False,line = line_mag))
-    
-    if show_phase_grid :
-        line_phase = dict(color = '#555', width = 1,dash = "dot")
-        
+            fig.add_trace(
+                go.Scatter(mag, hoverinfo="name", showlegend=False, line=line_mag)
+            )
+
+    if show_phase_grid:
+        line_phase = dict(color="#555", width=1, dash="dot")
+
         for phase in phase_list:
-            fig.add_trace(go.Scatter(phase,hoverinfo="name",showlegend=False,line = line_phase))
+            fig.add_trace(
+                go.Scatter(phase, hoverinfo="name", showlegend=False, line=line_phase)
+            )
 
     fig.show()
-
-
--- ENIB_control/utils.py
+++ ENIB_control/utils.py
@@ -4,7 +4,7 @@ from scipy.signal.ltisys import _default_response_times
 import control as ctl
 
 
-def get_T(tf_list,N=200,T=None):
+def get_T(tf_list, N=200, T=None):
     """ Get Time vector """
     if T is None:
         t_max = 0
@@ -16,6 +16,7 @@ def get_T(tf_list,N=200,T=None):
                 t_max = t_max_temp
                 T = T_temp
     return T
+
 
 def nichols_grid(cl_mags=None, cl_phases=None):
     """Nichols chart grid
@@ -36,17 +37,35 @@ def nichols_grid(cl_mags=None, cl_phases=None):
     ol_phase_max = 0.0
     ol_mag_min = -40.0
     ol_mag_max = default_ol_mag_max = 50.0
-    
+
     # M-circle magnitudes.
     if cl_mags is None:
         # Default chart magnitudes
-        key_cl_mags = np.array([-40.0, -20.0, -12.0, -6.0, -3.0, -1.0, -0.5, 0.0, 0.25, 0.5, 1.0, 3.0, 6.0, 12.0])
+        key_cl_mags = np.array(
+            [
+                -40.0,
+                -20.0,
+                -12.0,
+                -6.0,
+                -3.0,
+                -1.0,
+                -0.5,
+                0.0,
+                0.25,
+                0.5,
+                1.0,
+                3.0,
+                6.0,
+                12.0,
+            ]
+        )
         # Extend the range of magnitudes if necessary.
         cl_mag_step = -20.0  # dB
-        extended_cl_mags = np.arange(np.min(key_cl_mags),
-                                     ol_mag_min + cl_mag_step, cl_mag_step)
+        extended_cl_mags = np.arange(
+            np.min(key_cl_mags), ol_mag_min + cl_mag_step, cl_mag_step
+        )
         cl_mags = np.concatenate((extended_cl_mags, key_cl_mags))
-    else :
+    else:
         cl_mags = np.array(cl_mags)
 
     # N-circle phases (should be in the range -360 to 0)
@@ -60,22 +79,22 @@ def nichols_grid(cl_mags=None, cl_phases=None):
         cl_phases = np.concatenate((key_cl_phases, other_cl_phases))
     else:
         cl_phases = np.array(cl_phases)
-        assert ((-360.0 < np.min(cl_phases)) and (np.max(cl_phases) < 0.0))
-    
+        assert (-360.0 < np.min(cl_phases)) and (np.max(cl_phases) < 0.0)
+
     # Find the M-contours
     m = m_circles(cl_mags, phase_min=np.min(cl_phases), phase_max=np.max(cl_phases))
-    m_mag = 20*sp.log10(np.abs(m))
+    m_mag = 20 * sp.log10(np.abs(m))
     m_phase = sp.mod(sp.degrees(sp.angle(m)), -360.0)  # Unwrap
-    
+
     # Find the N-contours
     n = n_circles(cl_phases, mag_min=np.min(cl_mags), mag_max=np.max(cl_mags))
-    n_mag = 20*sp.log10(np.abs(n))
+    n_mag = 20 * sp.log10(np.abs(n))
     n_phase = sp.mod(sp.degrees(sp.angle(n)), -360.0)  # Unwrap
-    
+
     # Plot the contours behind other plot elements.
     # The "phase offset" is used to produce copies of the chart
-    phase_offset_min = 360.0*np.ceil(ol_phase_min/360.0)
-    phase_offset_max = 360.0*np.ceil(ol_phase_max/360.0) + 360.0
+    phase_offset_min = 360.0 * np.ceil(ol_phase_min / 360.0)
+    phase_offset_max = 360.0 * np.ceil(ol_phase_max / 360.0) + 360.0
     phase_offsets = np.arange(phase_offset_min, phase_offset_max, 360.0)
 
     data_m_mag = []
@@ -83,27 +102,38 @@ def nichols_grid(cl_mags=None, cl_phases=None):
     for phase_offset in phase_offsets:
         for indice in range(m_mag.shape[1]):
             name = "{} dB".format(cl_mags[indice])
-            data_m_mag.append({"x":m_phase[:,indice] + phase_offset,"y":m_mag[:,indice],"name":name})
+            data_m_mag.append(
+                {
+                    "x": m_phase[:, indice] + phase_offset,
+                    "y": m_mag[:, indice],
+                    "name": name,
+                }
+            )
         for indice in range(n_mag.shape[1]):
             name = "{} deg".format(cl_phases[indice])
-            data_n_mag.append({"x":n_phase[:,indice] + phase_offset,"y":n_mag[:,indice],"name":name})
+            data_n_mag.append(
+                {
+                    "x": n_phase[:, indice] + phase_offset,
+                    "y": n_mag[:, indice],
+                    "name": name,
+                }
+            )
 
-    return data_m_mag,data_n_mag
+    return data_m_mag, data_n_mag
 
 
 def closed_loop_contours(Gcl_mags, Gcl_phases):
-    Gcl = Gcl_mags*sp.exp(1.j*Gcl_phases)
-    return Gcl/(1.0 - Gcl)
+    Gcl = Gcl_mags * sp.exp(1.0j * Gcl_phases)
+    return Gcl / (1.0 - Gcl)
+
 
 def m_circles(mags, phase_min=-359.75, phase_max=-0.25):
     phases = sp.radians(sp.linspace(phase_min, phase_max, 2000))
-    Gcl_mags, Gcl_phases = sp.meshgrid(10.0**(mags/20.0), phases)
+    Gcl_mags, Gcl_phases = sp.meshgrid(10.0 ** (mags / 20.0), phases)
     return closed_loop_contours(Gcl_mags, Gcl_phases)
+
 
 def n_circles(phases, mag_min=-40.0, mag_max=12.0):
-    mags = sp.linspace(10**(mag_min/20.0), 10**(mag_max/20.0), 2000)
+    mags = sp.linspace(10 ** (mag_min / 20.0), 10 ** (mag_max / 20.0), 2000)
     Gcl_phases, Gcl_mags = sp.meshgrid(sp.radians(phases), mags)
     return closed_loop_contours(Gcl_mags, Gcl_phases)
-
-
-
